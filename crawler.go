d05c3bcc871d732bd52aa23defcf9f5596af4cec
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2018-08-20 02:17:27 +0530

    crawler functionality

--- crawler.go
+++ crawler.go
@@ -1,13 +1,75 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 func spiderman(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	websiteAddress := strings.TrimSpace(r.PostFormValue("website_address"))
 
+	content, err := scrapPage(websiteAddress)
+	if err != nil {
+		respondError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	input := strings.NewReader(string(content))
+	doc, err := goquery.NewDocumentFromReader(input)
+	if err != nil {
+		log.Println("could not scrap the page. err: ", err.Error())
+		respondError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	doc.Find("a").Each(func(index int, item *goquery.Selection) {
+		fmt.Println(item.Text())
+		href, ok := item.Attr("href")
+		if ok {
+			fmt.Println(href)
+		}
+	})
+
 	w.Write([]byte(websiteAddress))
+}
+
+// scrapPage takes url scraps the page return html string and error
+func scrapPage(urlStr string) (string, error) {
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		return "", err
+	}
+	// url.Scheme
+	// fmt.Println(url.Fragment)
+	// fmt.Println(url.Host)
+	// fmt.Println(url.Path)
+	// fmt.Println(url.RawPath)
+	// fmt.Println(url.RawQuery)
+
+	request, err := http.NewRequest(http.MethodGet, url.String(), nil)
+	if err != nil {
+		return "", err
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
 }
--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"path"
-	"strings"
-	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/gorilla/mux"
 )
 
@@ -26,26 +20,6 @@ func main() {
 	http.Handle("/", rtr)
 	log.Println("Starting server on port :8080")
 	http.ListenAndServe(":8080", nil)
-
-	content, err := scrapPage("https://www.redhat.com/")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	input := strings.NewReader(string(content))
-	doc, err := goquery.NewDocumentFromReader(input)
-	if err != nil {
-		log.Println("could not scrap the page. err: ", err.Error())
-		return
-	}
-
-	doc.Find("a").Each(func(index int, item *goquery.Selection) {
-		// fmt.Println(item.Text())
-		href, ok := item.Attr("href")
-		if ok {
-			fmt.Println(href)
-		}
-	})
 
 	return
 }
@@ -79,30 +53,4 @@ func HandleIndex(w http.ResponseWriter, r *http.Request) {
 func respondError(w http.ResponseWriter, err error, statusCode int) {
 	http.Error(w, err.Error(), statusCode)
 	return
-}
-
-// scrapPage takes url scraps the page return html string and error
-func scrapPage(urlStr string) (string, error) {
-	client := &http.Client{
-		Timeout: 15 * time.Second,
-	}
-
-	url, err := url.Parse(urlStr)
-	if err != nil {
-		return "", err
-	}
-	request, err := http.NewRequest(http.MethodGet, url.String(), nil)
-	if err != nil {
-		return "", err
-	}
-	response, err := client.Do(request)
-	if err != nil {
-		return "", err
-	}
-	data, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(data), nil
 }
