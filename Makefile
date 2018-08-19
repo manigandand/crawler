13587c165e5de29f51b5945f7975f0a6c9780d18
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2018-08-20 05:03:17 +0530

    craweler spider

---
+++ Makefile
@@ -0,0 +1,35 @@
+# Makefile
+
+GO_CMD=go
+GO_BUILD=$(GO_CMD) build
+GO_BUILD_RACE=$(GO_CMD) build -race
+GO_TEST=$(GO_CMD) test
+GO_TEST_VERBOSE=$(GO_CMD) test -v
+GO_TEST_COVER=$(GO_CMD) test -cover
+GO_INSTALL=$(GO_CMD) install -v
+
+SERVER_BIN=crawler
+SERVER_DIR=.
+SERVER_MAIN=main.go
+
+SOURCE_PKG_DIR=.
+SOURCEDIR=.
+SOURCES := $(shell find $(SOURCEDIR) -name '*.go')
+
+all: build-server run
+
+build-server:
+	@echo "==> Building server ...";
+	@$(GO_INSTALL)
+	@$(GO_BUILD)
+	@chmod 755 $(SERVER_BIN)
+
+test:
+	@echo "==> Running tests ...";
+	# @$(GO_TEST_COVER) $(SOURCE_PKG_DIR)/utils
+	# @$(GO_TEST_COVER) $(SOURCE_PKG_DIR)/ishell
+	# @$(GO_TEST_COVER) $(SOURCE_PKG_DIR)/models
+	# @$(GO_TEST_COVER) $(SOURCE_PKG_DIR)/store
+
+run:
+	./$(SERVER_BIN)
--- crawler.go
+++ crawler.go
@@ -1,44 +1,200 @@
 package main
 
 import (
+	"compress/gzip"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+// URL holds the basic site links
+type URL struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+	// IsVisited bool   `json:"is_visited"`
+}
+
+var (
+	SiteMapLock sync.RWMutex
+	// SiteMap holds all the links in the given site
+	SiteMap = make(map[string][]*URL)
+)
+
 func spiderman(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	websiteAddress := strings.TrimSpace(r.PostFormValue("website_address"))
-
-	content, err := scrapPage(websiteAddress)
+	baseURL, fullpath, err := getBaseURL(websiteAddress)
 	if err != nil {
 		respondError(w, err, http.StatusInternalServerError)
 		return
+	}
+
+	// check if the site already scraped
+	SiteMapLock.Lock()
+	if siteMap, ok := SiteMap[fullpath]; ok {
+		SiteMapLock.Unlock()
+		respondSuccess(w, siteMap)
+		return
+	}
+	SiteMapLock.Unlock()
+
+	urls, err := scraper(websiteAddress, baseURL)
+	if err != nil {
+		respondError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	// save in map
+	SiteMapLock.Lock()
+	SiteMap[fullpath] = urls
+	SiteMapLock.Unlock()
+	go traverseAllLinks(urls)
+
+	respondSuccess(w, urls)
+	return
+}
+
+func traverseAllLinks(urls []*URL) {
+	var wg sync.WaitGroup
+
+	maxNbConcurrentGoroutines := 5
+	concurrentGoroutines := make(chan struct{}, maxNbConcurrentGoroutines)
+	for i := 0; i < maxNbConcurrentGoroutines; i++ {
+		concurrentGoroutines <- struct{}{}
+	}
+	done := make(chan bool)
+	totalURLs := len(urls)
+
+	go func() {
+		for i := 0; i < totalURLs; i++ {
+			<-done
+			concurrentGoroutines <- struct{}{}
+		}
+	}()
+
+	wg.Add(totalURLs)
+	for _, u := range urls {
+		<-concurrentGoroutines
+		go webCrawler(u, &wg)
+	}
+
+	wg.Wait()
+	close(done)
+	return
+}
+
+func webCrawler(u *URL, wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+		// SiteMapLock.Unlock()
+	}()
+
+	SiteMapLock.Lock()
+	if _, ok := SiteMap[u.URL]; !ok {
+		SiteMapLock.Unlock()
+		baseURL, fullpath, err := getBaseURL(u.URL)
+		if err != nil {
+			return
+		}
+		urls, err := scraper(u.URL, baseURL)
+		if err != nil {
+			return
+		}
+
+		// save in map
+		SiteMapLock.Lock()
+		SiteMap[fullpath] = urls
+		SiteMapLock.Unlock()
+	}
+
+	return
+}
+
+func scraper(websiteAddress, baseURL string) ([]*URL, error) {
+	content, err := scrapPage(websiteAddress)
+	if err != nil {
+		return nil, err
 	}
 
 	input := strings.NewReader(string(content))
 	doc, err := goquery.NewDocumentFromReader(input)
 	if err != nil {
 		log.Println("could not scrap the page. err: ", err.Error())
+		return nil, err
+	}
+	var urls []*URL
+
+	doc.Find("a").Each(func(index int, item *goquery.Selection) {
+		// fmt.Println(item.Text())
+		href, ok := item.Attr("href")
+		if ok {
+			subURL, err := validateURL(strings.TrimSpace(href), baseURL)
+			if err == nil {
+				urls = append(urls, &URL{
+					Name: strings.TrimSpace(item.Text()),
+					URL:  strings.TrimSpace(subURL),
+				})
+			}
+			// fmt.Println(href)
+		}
+	})
+
+	return urls, nil
+}
+
+func respondSuccess(w http.ResponseWriter, data []*URL) {
+	gz := gzip.NewWriter(w)
+	defer gz.Close()
+
+	buf, err := json.Marshal(data)
+	if err != nil {
+		respondError(w, err, http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Content-Encoding", "gzip")
+	w.WriteHeader(http.StatusOK)
+	if _, err := gz.Write(buf); err != nil {
 		respondError(w, err, http.StatusInternalServerError)
 		return
 	}
 
-	doc.Find("a").Each(func(index int, item *goquery.Selection) {
-		fmt.Println(item.Text())
-		href, ok := item.Attr("href")
-		if ok {
-			fmt.Println(href)
-		}
-	})
+	return
+}
 
-	w.Write([]byte(websiteAddress))
+func validateURL(urlStr, baseURL string) (string, error) {
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		return "", err
+	}
+	if url.Scheme == "" && url.Host == "" {
+		return fmt.Sprintf("%s%s", baseURL, url.Path), nil
+	}
+
+	if strings.Contains(urlStr, baseURL) {
+		return fmt.Sprintf("%s%s", baseURL, url.Path), nil
+	}
+
+	return "", errors.New("invalid url")
+}
+
+func getBaseURL(urlStr string) (string, string, error) {
+	url, err := url.Parse(urlStr)
+	if err != nil {
+		return "", "", err
+	}
+
+	return fmt.Sprintf("%s://%s", url.Scheme, url.Host),
+		fmt.Sprintf("%s://%s%s", url.Scheme, url.Host, url.Path), nil
 }
 
 // scrapPage takes url scraps the page return html string and error
@@ -51,12 +207,6 @@ func scrapPage(urlStr string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	// url.Scheme
-	// fmt.Println(url.Fragment)
-	// fmt.Println(url.Host)
-	// fmt.Println(url.Path)
-	// fmt.Println(url.RawPath)
-	// fmt.Println(url.RawQuery)
 
 	request, err := http.NewRequest(http.MethodGet, url.String(), nil)
 	if err != nil {
